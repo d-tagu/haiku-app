652a57733a892fe6afc91a091eb6e621726652ec
d.taguchi <d.taguchi@i3design.co.jp>
2020-09-26 17:49:17 +0900

    Fix: TypeScript書き換え

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>haiku</title>
+      </Head>
+      <header></header>
+    </div>
+  )
+}
